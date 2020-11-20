33981b65d5fd251a08de4c12d17368e26ccdb32e
Damjan032 <damjan0032@gmail.com>
2020-11-20 22:26:17 +0100

    Spring init

---
+++ culture/src/test/java/rs/ac/uns/ftn/ktsnvt/culture/CultureApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.ac.uns.ftn.ktsnvt.culture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CultureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
