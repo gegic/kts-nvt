81245588ad9178543a62e31e097ea59cb375fe33
sloba2424 <sloba2424@gmail.com>
2021-01-21 18:23:21 +0100

    Fixed times on tests

--- kultura/back/src/test/java/rs/ac/uns/ftn/ktsnvt/kultura/e2e/CategoryE2ETest.java
+++ kultura/back/src/test/java/rs/ac/uns/ftn/ktsnvt/kultura/e2e/CategoryE2ETest.java
@@ -29,16 +29,16 @@ public class CategoryE2ETest {
 
         driver.get("http://localhost:4200/login");
 
-        justWait(2000);
+        justWait(1000);
 
         loginPage.getEmail().sendKeys("admin@mail.com");
         loginPage.getNextBtn().click();
-        justWait(2000);
+        justWait(1000);
         loginPage = PageFactory.initElements(driver, LoginPage.class);
-        justWait(2000);
+        justWait(1000);
         loginPage.getPassword().sendKeys("admin123");
         loginPage.getLoginBtn().click();
-        justWait(2000);
+        justWait(1000);
     }
 
     @After
@@ -79,87 +79,87 @@ public class CategoryE2ETest {
         justWait(1000);
 
         categoryPage.getNewCategoryBtn().click();
-        justWait(3000);
+        justWait(1000);
         categoryPage.getName().sendKeys(name);
 
         categoryPage.getSaveCategoryBtn().click();
-        justWait(3000);
+        justWait(1000);
     }
 
     @Test
     public void addAndDeleteCategory() throws InterruptedException {
-        justWait(3000);
+        justWait(1000);
         driver.get("http://localhost:4200/categories");
         JavascriptExecutor jse = (JavascriptExecutor) driver;
         String name = "TestCategory" + ThreadLocalRandom.current().nextInt(0, 9999 + 1);
         categoryPage = PageFactory.initElements(driver, CategoryPage.class);
         //skrol do vrha
-        justWait(3000);
+        justWait(1000);
         //jse.executeScript("window.scrollTo(0,0);");
         categoryPage.getNewCategoryBtn().click();
-        justWait(3000);
+        justWait(1000);
         categoryPage.getName().sendKeys(name);
-        justWait(3000);
+        justWait(1000);
         categoryPage.getSaveCategoryBtn().click();
-        justWait(3000);
+        justWait(1000);
         deleteCategory(name);
     }
 
     @Test
     public void editCategory() throws InterruptedException {
-        justWait(3000);
+        justWait(1000);
         driver.get("http://localhost:4200/categories");
         JavascriptExecutor jse = (JavascriptExecutor) driver;
         String name = "TestCategory" + ThreadLocalRandom.current().nextInt(0, 9999 + 1);
         categoryPage = PageFactory.initElements(driver, CategoryPage.class);
-        justWait(3000);
+        justWait(1000);
 
         categoryPage.getNewCategoryBtn().click();
-        justWait(3000);
+        justWait(1000);
         categoryPage.getName().sendKeys(name);
 
-        justWait(3000);
+        justWait(1000);
         categoryPage.getSaveCategoryBtn().click();
-        justWait(3000);
+        justWait(1000);
 
         jse.executeScript("window.scrollTo(0, document.body.scrollHeight)");
 
-        justWait(3000);
+        justWait(1000);
         categoryPage.ensureIsDisplayed("p" + name);
         categoryPage.editBtnCategory(name).click();
-        justWait(3000);
+        justWait(1000);
         categoryPage = PageFactory.initElements(driver, CategoryPage.class);
-        justWait(3000);
+        justWait(1000);
         categoryPage.getName().sendKeys("2");
-        justWait(3000);
+        justWait(1000);
         categoryPage.getElementById("addCategory").click();
-        justWait(3000);
+        justWait(1000);
         jse.executeScript("window.scrollTo(0, document.body.scrollHeight)");
-        justWait(3000);
+        justWait(1000);
         categoryPage.ensureIsDisplayed("p"+name+"2");
-        justWait(3000);
+        justWait(1000);
         deleteCategory(name+"2");
     }
 
     @Test
     public void addCategoryExists() throws InterruptedException {
-        justWait(3000);
+        justWait(1000);
         driver.get("http://localhost:4200/categories");
         JavascriptExecutor jse = (JavascriptExecutor) driver;
         String name = "Institucija";
         categoryPage = PageFactory.initElements(driver, CategoryPage.class);
-        justWait(3000);
+        justWait(1000);
         //jse.executeScript("window.scrollTo(0, 0)");
         driver.get("http://localhost:4200/categories");
-        justWait(3000);
+        justWait(1000);
 
         categoryPage.getNewCategoryBtn().click();
-        justWait(3000);
+        justWait(1000);
         categoryPage.getName().sendKeys(name);
 
-        justWait(3000);
+        justWait(1000);
         categoryPage.getSaveCategoryBtn().click();
-        justWait(3000);
+        justWait(1000);
 
         String toast = categoryPage.ensureIsDisplayedToast();
 
@@ -168,23 +168,23 @@ public class CategoryE2ETest {
 
     @Test
     public void addCategoryEmptyName() throws InterruptedException {
-        justWait(3000);
+        justWait(1000);
         driver.get("http://localhost:4200/categories");
         JavascriptExecutor jse = (JavascriptExecutor) driver;
         String name = "";
         categoryPage = PageFactory.initElements(driver, CategoryPage.class);
-        justWait(3000);
+        justWait(1000);
 //        jse.executeScript("window.scrollTo(0, 0)");
         driver.get("http://localhost:4200/categories");
-        justWait(3000);
+        justWait(1000);
 
         categoryPage.getNewCategoryBtn().click();
-        justWait(3000);
+        justWait(1000);
         categoryPage.getName().sendKeys(name);
 
-        justWait(3000);
+        justWait(1000);
         categoryPage.getSaveCategoryBtn().click();
-        justWait(3000);
+        justWait(1000);
 
         String toast = categoryPage.ensureIsDisplayedToast();
 
@@ -194,34 +194,34 @@ public class CategoryE2ETest {
 
     @Test
     public void editCategoryNameExists() throws InterruptedException {
-        justWait(3000);
+        justWait(1000);
         driver.get("http://localhost:4200/categories");
         JavascriptExecutor jse = (JavascriptExecutor) driver;
         String name = "TestCategory" + ThreadLocalRandom.current().nextInt(0, 9999 + 1);
         categoryPage = PageFactory.initElements(driver, CategoryPage.class);
-        justWait(3000);
+        justWait(1000);
 
         categoryPage.getNewCategoryBtn().click();
-        justWait(3000);
+        justWait(1000);
         categoryPage.getName().sendKeys(name);
 
         categoryPage.getSaveCategoryBtn().click();
-        justWait(3000);
+        justWait(1000);
         driver.get("http://localhost:4200/categories");
-        justWait(3000);
+        justWait(1000);
         jse.executeScript("window.scrollTo(0, document.body.scrollHeight)");
-        justWait(3000);
+        justWait(1000);
         categoryPage.ensureIsDisplayed("p" + name);
         categoryPage.editBtnCategory(name).click();
-        justWait(3000);
+        justWait(1000);
         categoryPage = PageFactory.initElements(driver, CategoryPage.class);
-        justWait(3000);
+        justWait(1000);
         categoryPage.getName().clear();
-        justWait(3000);
+        justWait(1000);
         categoryPage.getName().sendKeys("Institucija");
-        justWait(3000);
+        justWait(1000);
         categoryPage.getElementById("addCategory").click();
-        justWait(3000);
+        justWait(1000);
 
         String toast = categoryPage.ensureIsDisplayedToast();
 
@@ -230,20 +230,20 @@ public class CategoryE2ETest {
 
     @Test
     public void deleteCategoryWithSubcategories() throws InterruptedException {
-        justWait(3000);
+        justWait(1000);
         driver.get("http://localhost:4200/categories");
         JavascriptExecutor jse = (JavascriptExecutor) driver;
         String name = "Institucija";
 
-        justWait(3000);
+        justWait(1000);
         categoryPage = PageFactory.initElements(driver, CategoryPage.class);
-        justWait(3000);
+        justWait(1000);
 
         categoryPage.deleteBtnCategory(name).click();
-        justWait(3000);
+        justWait(1000);
         categoryPage.getConfirmationService().click();
         String toast = categoryPage.ensureIsDisplayedToast();
-        justWait(3000);
+        justWait(1000);
 
         assertEquals("Deletion unsuccessful\nThis category has subcategories associated" +
                 " with it. Firstly delete all its subcategories in order to be able to delete it.", toast);
@@ -251,40 +251,40 @@ public class CategoryE2ETest {
 
     @Test
     public void addCategoryAndSubcategories() throws InterruptedException {
-        justWait(3000);
+        justWait(1000);
         driver.get("http://localhost:4200/categories");
         JavascriptExecutor jse = (JavascriptExecutor) driver;
         String name = "TestCategory" + ThreadLocalRandom.current().nextInt(0, 9999 + 1);
         categoryPage = PageFactory.initElements(driver, CategoryPage.class);
-        justWait(3000);
+        justWait(1000);
 
         categoryPage.getNewCategoryBtn().click();
-        justWait(3000);
+        justWait(1000);
         categoryPage.getName().sendKeys(name);
 
-        justWait(3000);
+        justWait(1000);
         categoryPage.getSaveCategoryBtn().click();
-        justWait(3000);
+        justWait(1000);
         driver.get("http://localhost:4200/categories");
-        justWait(3000);
+        justWait(1000);
         jse.executeScript("window.scrollTo(0, document.body.scrollHeight)");
-        justWait(3000);
+        justWait(1000);
         categoryPage.ensureIsDisplayed("p" + name);
 
         categoryPage.subcategoriesBtnCategory(name).click();
-        justWait(3000);
+        justWait(1000);
 
         categoryPage.getNewSubcategoryBtn().click();
-        justWait(3000);
+        justWait(1000);
 
         categoryPage.getSubcategoryName().sendKeys("Subcategory1");
-        justWait(3000);
+        justWait(1000);
 
         categoryPage.getElementById("saveSubcategory").click();
-        justWait(3000);
+        justWait(1000);
 
         String toast = categoryPage.ensureIsDisplayedToast();
-        justWait(3000);
+        justWait(1000);
 
         assertEquals("Saved\nSubcategory was successfully saved.", toast);
 
@@ -297,35 +297,35 @@ public class CategoryE2ETest {
 //        executor.executeScript("arguments[0].click();", ele);
         //iz nekog razloga mora ovako, dijalog koji iskoci ga zbuni i nece da klikne...
         categoryPage.getElementById("deleteSubcategory1").sendKeys(Keys.RETURN);
-        justWait(3000);
+        justWait(1000);
         categoryPage.getConfirmationService().click();
-        justWait(3000);
+        justWait(1000);
         deleteCategory(name);
     }
 
     @Test
     public void addExistingSubcategory() throws InterruptedException {
-        justWait(3000);
+        justWait(1000);
         driver.get("http://localhost:4200/categories");
         JavascriptExecutor jse = (JavascriptExecutor) driver;
         String name = "Muzej";
         categoryPage = PageFactory.initElements(driver, CategoryPage.class);
-        justWait(3000);
+        justWait(1000);
 
         categoryPage.getElementById("subcategoriesInstitucija").click();
-        justWait(3000);
+        justWait(1000);
 
         categoryPage.getNewSubcategoryBtn().click();
-        justWait(3000);
+        justWait(1000);
 
         categoryPage.getSubcategoryName().sendKeys(name);
-        justWait(3000);
+        justWait(1000);
 
         categoryPage.getElementById("saveSubcategory").click();
-        justWait(3000);
+        justWait(1000);
 
         String toast = categoryPage.ensureIsDisplayedToast();
-        justWait(3000);
+        justWait(1000);
 
         assertEquals("Already exists\n" +
                 "A subcategory with this name already exists", toast);
@@ -333,114 +333,114 @@ public class CategoryE2ETest {
 
     @Test
     public void addSubcategoryEmptyName() throws InterruptedException {
-        justWait(3000);
+        justWait(1000);
         driver.get("http://localhost:4200/categories");
         JavascriptExecutor jse = (JavascriptExecutor) driver;
         String name = "";
         categoryPage = PageFactory.initElements(driver, CategoryPage.class);
-        justWait(3000);
+        justWait(1000);
 
         categoryPage.getElementById("subcategoriesInstitucija").click();
-        justWait(3000);
+        justWait(1000);
 
         categoryPage.getNewSubcategoryBtn().click();
-        justWait(3000);
+        justWait(1000);
 
         categoryPage.getSubcategoryName().sendKeys(name);
-        justWait(3000);
+        justWait(1000);
 
         categoryPage.getElementById("saveSubcategory").click();
-        justWait(3000);
+        justWait(1000);
 
         String toast = categoryPage.ensureIsDisplayedToast();
-        justWait(3000);
+        justWait(1000);
 
         assertEquals("Required\nName is required.", toast);
     }
 
     @Test
     public void deleteSubcategory() throws InterruptedException {
-        justWait(3000);
+        justWait(1000);
         driver.get("http://localhost:4200/categories");
         String categoryName = "TestCategory" + ThreadLocalRandom.current().nextInt(0, 9999 + 1);
         JavascriptExecutor jse = (JavascriptExecutor) driver;
         categoryPage = PageFactory.initElements(driver, CategoryPage.class);
         addCategory(categoryName);
-        justWait(3000);
+        justWait(1000);
 
         jse.executeScript("window.scrollTo(0, document.body.scrollHeight)");
 
-        justWait(3000);
+        justWait(1000);
         categoryPage.ensureIsDisplayed("p" + categoryName);
 
-        justWait(3000);
+        justWait(1000);
         categoryPage.subcategoriesBtnCategory(categoryName).click();
-        justWait(3000);
+        justWait(1000);
 
         categoryPage.getNewSubcategoryBtn().click();
-        justWait(3000);
+        justWait(1000);
 
         categoryPage.getSubcategoryName().sendKeys("Subcategory1");
-        justWait(3000);
+        justWait(1000);
 
         categoryPage.getElementById("saveSubcategory").click();
-        justWait(3000);
+        justWait(1000);
 
-        justWait(3000);
+        justWait(1000);
         categoryPage.deleteBtnSubcategory("Subcategory1").click();
-        justWait(3000);
+        justWait(1000);
         categoryPage.getConfirmationService().click();
 //        String toast = categoryPage.ensureIsDisplayedCustom("subcategory-deleted-message");
 //        categoryPage.ensureIsNonDisplayed("deleteSubcategory1");
         justWait(4000);
         String subcategoriesNumberText = categoryPage.getElementById("subcategoriesNumberText").getText();
 
-        justWait(3000);
+        justWait(1000);
         assertEquals(subcategoriesNumberText,
                 "In total there are 0 subcategories for "+categoryName+".");
     }
 
     @Test
     public void editSubcategory() throws InterruptedException {
-        justWait(3000);
+        justWait(1000);
         String categoryName = "TestCategory" + ThreadLocalRandom.current().nextInt(0, 9999 + 1);
         String subcategoryName = "TestSubcategory" + ThreadLocalRandom.current().nextInt(0, 9999 + 1);
         categoryPage = PageFactory.initElements(driver, CategoryPage.class);
         addCategory(categoryName);
-        justWait(3000);
+        justWait(1000);
 
         JavascriptExecutor jse = (JavascriptExecutor) driver;
         jse.executeScript("window.scrollTo(0, document.body.scrollHeight)");
 
-        justWait(3000);
+        justWait(1000);
         categoryPage.ensureIsDisplayed("p" + categoryName);
 
         categoryPage.subcategoriesBtnCategory(categoryName).click();
-        justWait(3000);
+        justWait(1000);
 
         categoryPage.getNewSubcategoryBtn().click();
-        justWait(3000);
+        justWait(1000);
 
         categoryPage.getSubcategoryName().sendKeys("Subcategory1");
-        justWait(3000);
+        justWait(1000);
 
         categoryPage.getElementById("saveSubcategory").click();
-        justWait(3000);
+        justWait(1000);
 
-        justWait(3000);
+        justWait(1000);
         categoryPage.editBtnSubcategory("Subcategory1").click();
-        justWait(3000);
+        justWait(1000);
         categoryPage.getSubcategoryName().clear();
-        justWait(3000);
+        justWait(1000);
 
         categoryPage.getSubcategoryName().sendKeys(subcategoryName);
-        justWait(3000);
+        justWait(1000);
 
         categoryPage.getElementById("saveSubcategory").click();
-        justWait(3000);
+        justWait(1000);
 
         String toast = categoryPage.ensureIsDisplayedToast();
-        justWait(3000);
+        justWait(1000);
 
         assertEquals("Saved\nSubcategory was successfully saved.", toast);
     }
@@ -493,44 +493,44 @@ public class CategoryE2ETest {
 
     @Test
     public void editSubcategoryNameExists() throws InterruptedException {
-        justWait(3000);
+        justWait(1000);
         String categoryName = "TestCategory" + ThreadLocalRandom.current().nextInt(0, 9999 + 1);
         String subcategoryName = "TestSubcategory" + ThreadLocalRandom.current().nextInt(0, 9999 + 1);
         categoryPage = PageFactory.initElements(driver, CategoryPage.class);
         addCategory(categoryName);
-        justWait(3000);
+        justWait(1000);
 
         JavascriptExecutor jse = (JavascriptExecutor) driver;
         jse.executeScript("window.scrollTo(0, document.body.scrollHeight)");
 
-        justWait(3000);
+        justWait(1000);
         categoryPage.ensureIsDisplayed("p" + categoryName);
 
         categoryPage.subcategoriesBtnCategory(categoryName).click();
-        justWait(3000);
+        justWait(1000);
 
         categoryPage.getNewSubcategoryBtn().click();
-        justWait(3000);
+        justWait(1000);
 
         categoryPage.getSubcategoryName().sendKeys("Subcategory1");
-        justWait(3000);
+        justWait(1000);
 
         categoryPage.getElementById("saveSubcategory").click();
-        justWait(3000);
+        justWait(1000);
 
         categoryPage.editBtnSubcategory("Subcategory1").click();
-        justWait(3000);
+        justWait(1000);
         categoryPage.getSubcategoryName().clear();
-        justWait(3000);
+        justWait(1000);
 
         categoryPage.getSubcategoryName().sendKeys(subcategoryName);
-        justWait(3000);
+        justWait(1000);
 
         categoryPage.getElementById("saveSubcategory").click();
-        justWait(3000);
+        justWait(1000);
 
         String toast = categoryPage.ensureIsDisplayedToast();
-        justWait(3000);
+        justWait(1000);
 
         assertEquals("Saved\nSubcategory was successfully saved.", toast);
     }
