f546e66e03a70755ff820cdfb1e3bef473ae89ca
Haris Gegić <harry.gegic@gmail.com>
2021-01-26 01:33:03 +0100

    k

--- kultura/frontNg/src/app/view/subcategories-view/subcategories-view.component.spec.ts
+++ kultura/frontNg/src/app/view/subcategories-view/subcategories-view.component.spec.ts
@@ -1,112 +1,164 @@
 import {ComponentFixture, fakeAsync, TestBed, tick} from '@angular/core/testing';
 
-import { SubcategoriesViewComponent } from './subcategories-view.component';
+import {SubcategoriesViewComponent} from './subcategories-view.component';
 import {DynamicDialogConfig, DynamicDialogModule, DynamicDialogRef} from 'primeng/dynamicdialog';
-import {TableModule} from 'primeng/table';
-import {Category} from '../../core/models/category';
-import {of} from 'rxjs';
 import {CategoryService} from '../../core/services/category/category.service';
-import {ConfirmationService, MessageService} from 'primeng/api';
-import {HttpClientModule} from '@angular/common/http';
+import Spy = jasmine.Spy;
 import {DialogModule} from 'primeng/dialog';
+import {ConfirmationService, MessageService} from 'primeng/api';
+import {Category} from '../../core/models/category';
+import createSpy = jasmine.createSpy;
+import {Subcategory} from '../../core/models/subcategory';
+import {of, throwError} from 'rxjs';
+import {Table, TableModule} from 'primeng/table';
+import {concatMap} from 'rxjs/operators';
 import {ToolbarModule} from 'primeng/toolbar';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import Spy = jasmine.Spy;
-import {By} from '@angular/platform-browser';
 
 describe('SubcategoriesViewComponent', () => {
   let component: SubcategoriesViewComponent;
   let fixture: ComponentFixture<SubcategoriesViewComponent>;
-  let dynamicDialogRef: any;
-  let dynamicDialogConfig: any;
-  let categoryService: any;
-  let messageService: any;
-  let confirmationService: any;
   let category: Category;
-  beforeEach( () => {
+  let categoryService: CategoryService;
+  let confirmationService: ConfirmationService;
+  let spyAdd: Spy;
+
+  beforeEach(async () => {
+    const subcategories = getSubcategories();
+    const mockCategoryService = {
+      getSubcategories: createSpy('getSubcategories').and.returnValue(of(subcategories)),
+      deleteSubcategory: createSpy('deleteSubcategory').and.returnValue(of()),
+      updateSubcategory: createSpy('updateSubcategory').and.returnValue(of()),
+      createSubcategory: createSpy('createSubcategory').and.returnValue(of())
+    };
+
     category = {
-      id: 71,
-      name: 'kategooorija',
-      numSubcategories: 3
+      id: 1,
+      name: 'Kategorija'
     };
-    const subcategories = {
-      content: [
-        {
-          id: 1,
-          name: 'Subcategory1',
-          numOfferings: 0,
-          categoryId: 71
-        },
-        {
-          id: 2,
-          name: 'Subcategory2',
-          categoryId: 71,
-          numOfferings: 1
-        },
-        {
-          id: 3,
-          name: 'Subcategory3',
-          categoryId: 71,
-          numOfferings: 2
-        },
-      ],
-      totalElements: 3
-    };
-    const categoryServiceMock = {
-      getSubcategories: jasmine.createSpy('getSubcategories').and.returnValue(of(subcategories))
-    };
-    const messageServiceMock = {
-      add: jasmine.createSpy('add').and.returnValue(of({}))
-    };
-    const confirmationServiceMock = {
-      confirm: jasmine.createSpy('confirm').and.returnValue(of())
-    };
-    const configMock = {
+    const mockConfig = {
       data: {category}
     };
-    TestBed.configureTestingModule({
+    await TestBed.configureTestingModule({
+      imports: [
+        DialogModule,
+        TableModule,
+        ToolbarModule,
+        FormsModule,
+        ReactiveFormsModule
+      ],
       declarations: [ SubcategoriesViewComponent ],
-      imports: [DynamicDialogModule, TableModule, HttpClientModule, DialogModule,
-        ToolbarModule, ReactiveFormsModule, FormsModule],
       providers: [
-        DynamicDialogRef,
-        { provide: DynamicDialogConfig, useValue: configMock },
-        { provide: MessageService, useValue: messageServiceMock },
-        { provide: ConfirmationService, useValue: confirmationServiceMock },
-        { provide: CategoryService, useValue: categoryServiceMock },
-      ]
+        MessageService,
+        ConfirmationService,
+        {provide: DynamicDialogRef, useValue: {}},
+        {provide: DynamicDialogConfig, useValue: mockConfig},
+        {provide: CategoryService, useValue: mockCategoryService}
+      ],
     })
-      .compileComponents();
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    categoryService = TestBed.inject(CategoryService);
+    spyAdd = spyOn(TestBed.inject(MessageService), 'add');
+    confirmationService = TestBed.inject(ConfirmationService);
     fixture = TestBed.createComponent(SubcategoriesViewComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-    categoryService = TestBed.inject(CategoryService);
-    messageService = TestBed.inject(MessageService);
-    confirmationService = TestBed.inject(ConfirmationService);
-    dynamicDialogRef = TestBed.inject(DynamicDialogRef);
-    dynamicDialogConfig = TestBed.inject(DynamicDialogConfig);
   });
-
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-  it('should load category on init', fakeAsync(() => {
-    component.ngOnInit();
-    fixture.detectChanges();
-    tick();
-    expect(component.category).toEqual(category);
-    // expect(dynamicDialogConfig.data.category).toEqual(category);
-  }));
-  it('should render table', fakeAsync (() => {
-    component.ngOnInit();
-    fixture.detectChanges();
-    tick();
 
-    // const result = fixture.debugElement.query(By.css('.subcategoriesTable')).nativeElement;
-    fixture.detectChanges();
+  it('should init category from refconfig', () => {
+    expect(component.category).toEqual(category);
+  });
+
+  it('should get subcategories', fakeAsync(() => {
+    component.getSubcategories({first: 0, rows: 10});
+
+    expect(component.isSubcategoriesLoading).toBeTrue();
     tick();
-    const tableRows = fixture.nativeElement.querySelectorAll('tr');
-    expect(tableRows.length).toBe(4);
+    expect(categoryService.getSubcategories).toHaveBeenCalledWith(1, 0);
+    expect(component.subcategories).toEqual(getSubcategories());
+    expect(component.isSubcategoriesLoading).toBeFalse();
   }));
+  //
+  // it('should open subcategories dialog', () => {
+  //   component.openNew();
+  //
+  //   expect(component.isSubcategoryDialogOpen).toBeTrue();
+  // });
+  //
+  // it('should set subcategory for editing', () => {
+  //   component.editSubcategory(getSubcategories()[0]);
+  //
+  //   expect(component.editingSubcategory).toEqual(getSubcategories()[0]);
+  //   expect(component.isSubcategoryDialogOpen).toBeTrue();
+  //   expect(component.nameControl.value).toEqual(getSubcategories()[0].name);
+  // });
+  //
+  // it('should set subcategory for deletion', () => {
+  //   const spyConfirm = spyOn(confirmationService, 'confirm').and.callFake(confirmation => {
+  //     return confirmation.accept();
+  //   });
+  //   const spyDeletionConfirmed = spyOn(component, 'deletionConfirmed');
+  //
+  //   component.deleteSubcategory(getSubcategories()[0]);
+  //
+  //   expect(spyConfirm).toHaveBeenCalled();
+  //   expect(spyDeletionConfirmed).toHaveBeenCalledWith(getSubcategories()[0]);
+  // });
+  //
+  // it('should delete subcategory after confirmation', fakeAsync(() => {
+  //   const spyResetSubcategories = spyOn(component, 'resetSubcategories');
+  //
+  //   component.deletionConfirmed(getSubcategories()[0]);
+  //
+  //   tick();
+  //   expect(categoryService.deleteSubcategory).toHaveBeenCalledWith(1);
+  //   expect(spyAdd).toHaveBeenCalled();
+  //   expect(spyResetSubcategories).toHaveBeenCalled();
+  // }));
+  //
+  // it('should fail deleting subcategory after confirmation', fakeAsync(() => {
+  //   const spyResetSubcategories = spyOn(component, 'resetSubcategories');
+  //   categoryService.deleteSubcategory = createSpy('deleteSubcategory').and
+  //     .returnValue(throwError(null));
+  //   component.deletionConfirmed(getSubcategories()[0]);
+  //
+  //   tick();
+  //   expect(categoryService.deleteSubcategory).toHaveBeenCalledWith(1);
+  //   expect(spyAdd).toHaveBeenCalled();
+  //   expect(spyResetSubcategories).not.toHaveBeenCalled();
+  // }));
+  //
+  // it('should reset table', fakeAsync(() => {
+  //   component.resetSubcategories();
+  //
+  //   tick();
+  //   fixture.detectChanges();
+  //
+  //   expect((component.table as Table).value.length).toEqual(0);
+  // }));
+  //
+  // it('should save a new subcategory', fakeAsync(() => {
+  //   component.nameControl.setValue('Potkategorija');
+  //   component.saveSubcategory();
+  //
+  //   tick();
+  //   fixture.detectChanges();
+  //
+  //   expect(categoryService.createSubcategory).toHaveBeenCalledWith(jasmine.objectContaining({name: 'Potkategorija'}))
+  // }));
 });
+
+function getSubcategories(): Subcategory[] {
+  return [
+    {id: 1, categoryId: 1, name: 'potk1'},
+    {id: 2, categoryId: 1, name: 'potk2'}
+  ];
+}
+
